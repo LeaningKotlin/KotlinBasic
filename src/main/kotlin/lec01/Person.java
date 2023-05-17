ab1a88c7a71ab34aa8ea37d4fc6f981a11274202
GGos3 <janpll0421@gmail.com>
2023-05-17 14:30:50 +0900

    getAge 메서드 추가 및 생성자 추가

--- src/main/kotlin/lec01/Person.java
+++ src/main/kotlin/lec01/Person.java
@@ -4,13 +4,23 @@ import org.jetbrains.annotations.Nullable;
 
 public class Person {
     private String name;
+    private int age;
 
     @Nullable
     public String getName() {
         return name;
     }
 
+    public int getAge() {
+        return age;
+    }
+
     public Person(String name) {
         this.name = name;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 }
