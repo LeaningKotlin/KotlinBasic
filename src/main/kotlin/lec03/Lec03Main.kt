14b4028e086c01a2862f2cc2e3eb241a61e79cd5
GGos3 <janpll0421@gmail.com>
2023-05-17 14:47:40 +0900

    코틀린 StringFormat 및 indexSlicing 실습

--- src/main/kotlin/lec03/Lec03Main.kt
+++ src/main/kotlin/lec03/Lec03Main.kt
@@ -10,6 +10,11 @@ fun main() {
 
     val person = Person("김장필", 19)
     otherPrintAgeIsPerson(person)
+    printPersonInfo(person)
+
+    val str = "테스트용 문자열 입니다."
+    val indexSlicedChar = indexSlice(str, 3)
+    println(indexSlicedChar)
 }
 
 fun printAgeIsPerson(obj: Any) {
@@ -22,4 +27,15 @@ fun printAgeIsPerson(obj: Any) {
 fun otherPrintAgeIsPerson(obj: Any?) {
     val person = obj as? Person // obj 가 Null 이라면 코드 전체가 Null이 된다.
     println(person?.age) // person이 null 이기 때문에 null이 출력된다.
+}
+
+fun printPersonInfo(person: Person) {
+    // 기존 자바 코드
+    System.out.println(String.format("이름은 : %s 나이는 %d", person.name, person.age))
+    // 코틀린 변환
+    println("이름은 : ${person.name} 나이는 ${person.age}")
+}
+
+fun indexSlice(str: String, index: Int): Char {
+    return str[index]
 }
